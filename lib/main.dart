09eebcc04a29bcb486aa10e3c2276ff3ad03f026
zero921123 <zero921123@126.com>
2019-04-12 13:45:25 +0800

    JBZJ

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'TabPages/HomePage.dart';
+import 'JBZJ/JBZJ.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,13 +7,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5), //按钮被点击的高量颜色
         splashColor: Colors.blue, //点击按钮的博文颜色
         primarySwatch: Colors.green,
       ),
-      home: HomePage(),
+      home: JbzjDemo(),
 
     );
   }
